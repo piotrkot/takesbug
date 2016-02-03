83549a21da6d983377c4fa1845f00481fc70c925
Piotr Kotlicki <piotr.kotlicki@gmail.com>
2016-02-03 08:43:14 +0100

    Takes' form parameter bug.

--- src/main/java/com/github/piotrkot/Bug.java
+++ src/main/java/com/github/piotrkot/Bug.java
@@ -1,11 +1,14 @@
 package com.github.piotrkot;
 
 import java.io.IOException;
+import org.takes.Take;
 import org.takes.facets.fork.FkRegex;
 import org.takes.facets.fork.TkFork;
 import org.takes.http.Exit;
 import org.takes.http.FtBasic;
+import org.takes.rq.RqForm;
 import org.takes.rs.RsHTML;
+import org.takes.rs.RsVelocity;
 
 /**
  * Bug.
@@ -20,12 +23,26 @@ public final class Bug {
      */
     public static void main(final String... args) throws IOException {
         new FtBasic(
-                new TkFork(
-                    new FkRegex(
-                        "/",
-                        new RsHTML(Bug.class.getResourceAsStream("/bootstrap.html"))
-                    )
+            new TkFork(
+                new FkRegex(
+                    "/",
+                    new RsHTML(Bug.class.getResourceAsStream("/bootstrap.html"))
                 ),
+                new FkRegex(
+                    "/form",
+                    new RsHTML(Bug.class.getResourceAsStream("/form.html"))
+                ),
+                new FkRegex(
+                    "/hello",
+                    (Take) req -> new RsVelocity(
+                        "Hi, ${user}!",
+                        new RsVelocity.Pair(
+                            "user",
+                            new RqForm.Base(req).param("user").iterator().next()
+                        )
+                    )
+                )
+            ),
             8080
         ).start(Exit.NEVER);
     }
